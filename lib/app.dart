996dab9e9beea405ae8f95292b8f92e669a67b3b
denihero <denihero81@gmail.com>
2022-09-02 22:55:50 +0600

    small fix

---
+++ lib/app.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:http_practice/presentation/bloc/auth/auth_bloc.dart';
+import 'package:http_practice/presentation/bloc/clothes_cubit.dart';
+import 'package:http_practice/presentation/screens/cart_screen/cart_list_product_provider.dart';
+import 'package:http_practice/presentation/screens/cart_screen/cart_screen.dart';
+import 'package:http_practice/presentation/screens/home_screen/home_screen.dart';
+import 'package:http_practice/presentation/screens/home_screen/model/api_request/api_request.dart';
+import 'package:http_practice/presentation/screens/login/login_screen.dart';
+import 'package:http_practice/presentation/screens/register/register_screen.dart';
+import 'package:provider/provider.dart';
+
+import 'behavior.dart';
+
+class ShopApp extends StatelessWidget {
+  const ShopApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ChangeNotifierProvider(
+      create: (context) => ListCartProduct(),
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<ClothesCubit>(
+            create: (context) => ClothesCubit(apiRequest: ApiRequest()),
+          ),
+          BlocProvider<AuthBloc>(
+            create: (context) => AuthBloc(),
+          ),
+        ],
+        child: MaterialApp(
+          scrollBehavior: MyBehavior(),
+          debugShowCheckedModeBanner: false,
+          initialRoute: '/login',
+          routes: {
+            '/': (context) => const HomeScreen(),
+            '/login': (context) => const LoginScreen(),
+            '/cart_screen': (context) => const CartScreen(),
+            '/register_screen': (context) => const RegisterScreen()
+          },
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:http_practice/behavior.dart';
-import 'package:http_practice/presentation/bloc/auth/auth_bloc.dart';
-import 'package:http_practice/presentation/bloc/clothes_cubit.dart';
-import 'package:http_practice/presentation/screens/cart_screen/cart_screen.dart';
-import 'package:http_practice/presentation/screens/cart_screen/cart_list_product_provider.dart';
-import 'package:http_practice/presentation/screens/home_screen/home_screen.dart';
-import 'package:http_practice/presentation/screens/home_screen/model/api_request/api_request.dart';
-import 'package:http_practice/presentation/screens/login/login_screen.dart';
-import 'package:http_practice/presentation/screens/register/register_screen.dart';
-import 'package:provider/provider.dart';
-
+import 'app.dart';
 import 'firebase_options.dart';
 
 Future<void> main() async {
@@ -19,46 +8,6 @@ Future<void> main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
-  runApp(const App());
+  runApp(const ShopApp());
 }
 
-class App extends StatelessWidget {
-  const App({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return const ShopApp();
-  }
-}
-
-class ShopApp extends StatelessWidget {
-  const ShopApp({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: (context) => ListCartProduct(),
-      child: MultiBlocProvider(
-        providers: [
-          BlocProvider<ClothesCubit>(
-            create: (context) => ClothesCubit(apiRequest: ApiRequest()),
-          ),
-          BlocProvider<AuthBloc>(
-            create: (context) => AuthBloc(),
-          ),
-        ],
-        child: MaterialApp(
-          scrollBehavior: MyBehavior(),
-          debugShowCheckedModeBanner: false,
-          initialRoute: '/login',
-          routes: {
-            '/': (context) => const HomeScreen(),
-            '/login': (context) => const LoginScreen(),
-            '/cart_screen': (context) => const CartScreen(),
-            '/register_screen': (context) => const RegisterScreen()
-          },
-        ),
-      ),
-    );
-  }
-}
